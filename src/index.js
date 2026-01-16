6ea30cfcf4566a08bf01752d020208418c7727a5
WysemanTalal <talalazeem006@gmail.com>
2026-01-16 17:35:24 +0530

    inital commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render("login");
+});
+
+app.get('/signup', (req, res) => {
+    res.render("singup")
+})
+
+
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+})
